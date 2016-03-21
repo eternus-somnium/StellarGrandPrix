2480f885479f6ed394d67e920b51be0bdaf58639
DoomTay <bluejedi@cfl.rr.com>
2016-03-21 16:56:53 -0400

    Added basic ship movement

---
+++ StellarGrandPrix/Assets/MainBody.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBody : MonoBehaviour {
+
+	public float speed = 60;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce (transform.forward * speed * Input.GetAxis ("Vertical"));
+		rb.AddForce (transform.right * speed * Input.GetAxis ("Horizontal"));
+		rb.AddTorque (transform.up * speed * Input.GetAxis ("Yaw"));
+		rb.AddTorque (transform.right * speed * Input.GetAxis ("Pitch"));
+	}
+}
