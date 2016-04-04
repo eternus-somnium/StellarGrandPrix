f0c703372d52d002e1ba8e699e3a4e4843c0fcdd
DoomTay <bluejedi@cfl.rr.com>
2016-04-04 17:10:45 -0400

    Added Thruster Integration

--- StellarGrandPrix/Assets/MainBody.cs
+++ StellarGrandPrix/Assets/MainBody.cs
@@ -5,6 +5,11 @@ public class MainBody : MonoBehaviour {
 
 	public float speed = 60;
 	private Rigidbody rb;
+	private Vector3 directionVector;
+	public GameObject[] forwardThurusters;
+	public GameObject[] leftThrusters;
+	public GameObject[] rightThrusters;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -12,10 +17,16 @@ public class MainBody : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rb.AddForce (transform.forward * speed * Input.GetAxis ("Vertical"));
+		/*rb.AddForce (transform.forward * speed * Input.GetAxis ("Vertical"));
 		rb.AddForce (transform.right * speed * Input.GetAxis ("Horizontal"));
 		rb.AddTorque (transform.up * speed * Input.GetAxis ("Yaw"));
 		rb.AddTorque (transform.forward * speed * Input.GetAxis ("Roll"));
-		rb.AddTorque (transform.right * speed * Input.GetAxis ("Pitch"));
+		rb.AddTorque (transform.right * speed * Input.GetAxis ("Pitch"));*/
+		foreach (GameObject fwd in forwardThurusters) {
+			fwd.GetComponent<Rigidbody> ().AddForce (transform.forward * speed * Input.GetAxis ("Vertical"));
+		}
+		foreach (GameObject rightThruster in rightThrusters) {
+			rightThruster.GetComponent<Rigidbody> ().AddForce (-transform.forward * speed * 0.5f * Input.GetAxis ("Horizontal"));
+		}
 	}
 }
