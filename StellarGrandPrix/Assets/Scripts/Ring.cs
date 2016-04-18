969b10a28fa40f162b6564c05b97ca24ad88732e
DoomTay <bluejedi@cfl.rr.com>
2016-04-18 17:37:04 -0400

    Added ring direction detection and moved scripts to scripts folder

---
+++ StellarGrandPrix/Assets/Scripts/Ring.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ring : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit (Collider col) {
+		if (col.GetComponent<Rigidbody> ()) {
+			if (Vector3.Dot(transform.forward, col.GetComponent<Rigidbody> ().velocity) < 0) print("Passed the right way");
+			else if (Vector3.Dot(transform.forward, col.GetComponent<Rigidbody> ().velocity) > 0) print("Passed the wrong way");
+		}
+	}
+}
