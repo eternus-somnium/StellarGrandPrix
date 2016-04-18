bba4147fd79ef278782e042d835ac67fb96577ba
Nicoli20 <williamsnsjx@knights.ucf.edu>
2016-04-18 17:38:10 -0400

    Added Pausing and Resuming the Game
    The "P" key pauses and resumes the game in the prototype scene.

---
+++ StellarGrandPrix/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    private bool isPaused;
+	
+    // Use this for initialization
+	void Start ()
+    {
+	    if (Time.timeScale != 0)
+            isPaused = false;
+        else
+            isPaused = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // "P", the pause button, was pressed.
+        if (Input.GetKeyDown("p"))
+        {
+            if (isPaused == false)
+                PauseGame();
+            else
+                ResumeGame();
+        }
+	}
+
+    // Pauses the game.
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    // Resumes the game.
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+}
